811eff78450ae8c4f4deaea5583cdad1f1091868
terminalPoltergeist <nemit031@umn.edu>
2022-01-05 17:36:57 -0600

    feat: gather and pipe git log data to cache
    - create .commitment directory in home directory
    - gather, format, and pipe git log data to log.csv

--- commitment/__main__.py
+++ commitment/__main__.py
@@ -1,11 +1,17 @@
 import sys
+import os
 import argparse
 from .verification import verify
 
 def main():
     parser = argparse.ArgumentParser(description='Display git commit history in a graphical interface.', epilog='More: https://github.com/terminalPoltergeist/commitment.git')
     args = parser.parse_args()
-    verify()
+    home_dir = os.path.expanduser('~') 
+    git_repo = verify()
+    if git_repo != "":
+        os.system("mkdir " + home_dir + "/.commitment")
+        os.system("git log --pretty=format:'%h,%an,%ar,%s' > " + home_dir + "/.commitment/log.csv")
+
 
 if __name__ == '__main__':
     main()
--- commitment/verification.py
+++ commitment/verification.py
@@ -1,15 +1,14 @@
 import os.path
 
+parent = ""
 def verify():
     # current working directory
     path = os.path.abspath(os.getcwd())
     dirs = path.split("/")
-    parent = path
     for i in range(len(dirs)):
         if os.path.isdir(path + "/.git/"):
             parent = path 
             break
         path = '/'.join(dirs[:-i])
-    return os.path.isdir(parent + "/.git/")
-print(verify())
+    return parent
 
