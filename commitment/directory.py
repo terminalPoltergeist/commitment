cd5f66eb9b7970ec451f09640f99c4092647ea06
terminalPoltergeist <nemit031@umn.edu>
2022-01-17 15:21:28 -0600

    add: get commit history for all branches

--- commitment/directory.py
+++ commitment/directory.py
@@ -4,6 +4,6 @@ def directory(path):
     home_dir = os.path.expanduser('~') 
     if not os.path.isdir(home_dir + "/.commitment"):
         os.system("mkdir " + home_dir + "/.commitment")
-    os.system("git log --pretty=format:'%h,%an,%ar,%s' > " + home_dir + "/.commitment/log.csv")
+    os.system("git log --pretty=format:'%h,%an,%ar,%s' --all > " + home_dir + "/.commitment/log.csv")
     
 
