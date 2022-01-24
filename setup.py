04dc5a7dffbc2632c5bebb875629d53989ced4b8
terminalPoltergeist <nemit031@umn.edu>
2022-01-23 18:20:43 -0600

    add: Included licence and project url

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ setup(
     name = 'commitment',
     version = '0.0.1',
     packages = ['commitment'],
+    license = 'MIT',
+    url = 'https://github.com/terminalPoltergeist/commitment',
     entry_points = {
         'console_scripts': [
             'commitment = commitment.__main__:main'
